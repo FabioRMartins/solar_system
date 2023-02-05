e5e6208953008ffcb794d3ebcc2768470f7a58b5
FabioMartins <fabbio1000@gmail.com>
2023-02-05 11:12:51 -0300

    atualizando

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
 import Missions from './components/Missions';
 import MissionCard from './components/MissionCard';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <div className="main-container">
         <Header />
         <SolarSystem />
         <Missions />
         <Title />
         <MissionCard />
-      </>
+      </div>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Header extends React.Component {
+export default class Header extends React.Component {
   render() {
     return (
-      <header>
-        <h1>Sistema Solar</h1>
+      <header className="header">
+        <h1 className="header-title">Sistema Solar</h1>
       </header>
     );
   }
 }
-
-export default Header;
